e7bdfddfb4184e9b976b2196463bd7ec76b055fc
NxtGenLegend <62510055+NxtGenLegend@users.noreply.github.com>
2022-07-31 01:08:24 +0530

    Added arrays for results & accuracy, removed sklearn cnn attempts & shifted imports to one cell, two codes need to be merged & final decision needs to be displayed, then we're good to go

--- CNNupdated.py
+++ CNNupdated.py
@@ -19,6 +19,9 @@ from tensorflow.keras.applications.inception_v3 import preprocess_input
 from keras.layers import Convolution2D,Dense,MaxPool2D,Activation,Dropout,Flatten
 from keras.layers import Input, Add, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D, AveragePooling2D, MaxPooling2D, GlobalMaxPooling2D
 
+resultsdic = {}
+cnnreslist = []
+
 model = Sequential()
 model.add(Conv2D(32, (5, 5),input_shape=input_shape,activation='relu',name="conv2d_1"))
 model.add(MaxPooling2D(pool_size=(3, 3),name="max_pooling2d_1"))
@@ -68,12 +71,16 @@ def prepare(img_path):
     x = image.img_to_array(img)
     x = x/255
     return np.expand_dims(x, axis=0)
-    
-img_url='/content/drive/MyDrive/Colab Notebooks/dataset/plant__leaf/val/Apple__Healthy/78e648c6-a360-4fa8-b8ab-1225b164b7fd___RS_HL 7243.JPG'
 
-result_cnn = model_cnn.predict([prepare(img_url)])
-disease=image.load_img(img_url)
-plt.imshow(disease)
+#take testsetsglobal from the models code in order to test
 
-classresult=np.argmax(result_cnn,axis=1)
-print(classes[classresult[0]])
+for filename in os.scandir(testsetsglobal):
+    img_url = filename.path
+    #img_url='/content/drive/MyDrive/Colab Notebooks/dataset/plant__leaf/val/Apple__Healthy/78e648c6-a360-4fa8-b8ab-1225b164b7fd___RS_HL 7243.JPG'
+    result_cnn = model_cnn.predict([prepare(img_url)])
+    disease=image.load_img(img_url)
+    plt.imshow(disease)
+    classresult=np.argmax(result_cnn,axis=1)
+    cnnreslist.append(classes[classresult[0]])
+    resultsdic["cnn"] = cnnreslist
+    print(classes[classresult[0]])
